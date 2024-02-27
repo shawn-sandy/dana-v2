1153c4819226210723963a6f3a7c37b9b9aa524e
shawn <shawnsandy04@gmail.com>
2024-02-27 08:25:05 -0500

    refactor: migrate to astro:content and update markdown posts
    - Migrate to astro:content from glob for fetching posts
    - Update post page to use slug instead of url
    - Update blog page to use slug for post links
    - Remove hardcoded sample markdown post
    - Update markdown post layout not to render title
    - Fix formatting in social component
    - Add astro types reference

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
-    title: "Astro Learner | Blog",
+    title: "Astro Kit | Blog",
     description: "My journey learning Astro",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubData: post.data.pubDate,
+      description: post.data.description,
+      link: `post/{post.slug}`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
