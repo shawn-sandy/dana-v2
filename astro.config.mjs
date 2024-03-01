1c2da6217e7b85d4196657e6355833436738c862
shawn <shawnsandy04@gmail.com>
2024-03-01 16:31:56 -0500

    feat: add table of contents and emoji plugins
    Add remark-toc and rehype-accessible-emojis plugins. Configure shiki theme and syntax highlighting. Upgrade @fpkit/react dependency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
@@ -10,4 +11,15 @@ export default defineConfig({
   integrations: [react(), mdx()],
   adapter: netlify(),
   output: "hybrid",
+  // Enable Custom Markdown options, plugins, etc.
+  markdown: {
+    syntaxHighlight: "shiki",
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [rehypeAccessibleEmojis],
+    shikiConfig: {
+      theme: "dracula",
+      // Learn more about this configuration here:
+      // https://docs.astro.build/en/guides/markdown-content/#syntax-highlighting
+    },
+  },
 });
