53885fb6e86849a3f08a38f0813b7a3454a48f6a
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2024-03-17 17:17:13 -0400

    Feat/kit-docs (#21)
    * chore: remove mdx example file
    
    - The mdx-example.mdx file was deleted.
    - This file contained an example MDX blog post with features like multiple authors, published date, syntax highlighting, etc.
    - Removing this sample content file that is not needed anymore.
    
    * chore: remove mdx example file
    
    * feat: add docs section and update pagination
    
    - Add docs section with new docs page and doc post pages
    - Update pagination and sorting for docs and posts
    - Update breadcrumb and routes to handle docs section
    
    * fix: resolve breadcrumb routing issues
    
    - The breadcrumb component was updated to better handle dynamic routes from Astro.
    - Added prop passing to share Astro routes with the Breadcrumb component.
    - Updated handling of breadcrumb routes to properly merge Astro and React routes.
    - Removed unused route definition from docs page.
    - Updated docs page slug to match file name.
    
    * docs: remove introduction page
    
    - The introduction page located at src/content/docs/introduction.mdx was removed.
    - The docs/[page].astro file was also updated by removing some content (represented by .....).
    
    * feat: paginate docs pages
    
    - Paginate docs pages and increase page size to 5.
    - Remove docs page sorting. Update Pagination component to point to /docs route.
    
    * feat: paginate docs home page to show first 5 posts
    
    - Paginated the docs home page to initially show the first 5 doc posts.
    - Added a 'Continue reading docs' link to load the remaining posts on a separate paginated page.
    
    * fix: limit homepage docs to 3 and update pagination
    
    The diff makes two changes:
    
    - Limits the number of docs shown on the homepage to 3
    - Updates the pagination link to match the new limited docs

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,6 +26,33 @@ const postsCollection = defineCollection({
   }),
 });
 
+const astroKitDocs = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    breadcrumbSlug: z.string().optional(), // should match the slug
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    tags: z.array(z.string()),
+    isDraft: z.boolean().default(false),
+    youtube: z
+      .object({
+        id: z.string(),
+        title: z.string().optional(),
+        start: z.string().optional(),
+        end: z.string().optional(),
+      })
+      .optional(),
+  }),
+});
+
 export const collections = {
   posts: postsCollection,
+  docs: astroKitDocs,
 };
